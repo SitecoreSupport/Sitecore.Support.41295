dc42cbb49638690c143ad5b8772f5edc3ab4720c
DK\mao <mao@sitecore.net>
2018-09-11 13:35:28 +0300

    Initila commit to merge with Sitecore.Support.40925 patch.

---
+++ src/Sitecore.Support.41295/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.41295")]
+[assembly: AssemblyProduct("Sitecore.Support.41295")]
+[assembly: ComVisible(false)]
